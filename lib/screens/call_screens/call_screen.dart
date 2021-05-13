0dbfc3f2ec7d2bd17ac4664d770a8550f3da5184
taiwo_kenny <taiwokenny45@gmail.com>
2021-05-13 15:04:22 +0100

    pick up screen

---
+++ lib/screens/call_screens/call_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CallScreen extends StatefulWidget {
+  const CallScreen({Key key}) : super(key: key);
+
+  @override
+  _CallScreenState createState() => _CallScreenState();
+}
+
+class _CallScreenState extends State<CallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/call_screens/pick_up/pickup_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:skype_clone/models/call.dart';
+import 'package:skype_clone/resources/call_methods.dart';
+
+class PickUpScreen extends StatelessWidget {
+  final Call call;
+  final callMethods = CallMethods();
+
+  const PickUpScreen({Key key, this.call}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(vertical: 100),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Incoming...",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Image.network(
+                    call.callerPic,
+                    height: 150,
+                    width: 150,
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    call.callerName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 75),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.call_end),
+                        color: Colors.redAccent,
+                        onPressed: () async {
+                          // isCallMissed = false;
+                          // addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
+                          await callMethods.endCall(call: call);
+                        },
+                      ),
+                      SizedBox(width: 25),
+                      IconButton(
+                          icon: Icon(Icons.call),
+                          color: Colors.green,
+                          onPressed: () async {
+                            // isCallMissed = false;
+                            // addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
+                            // await Permissions.cameraAndMicrophonePermissionsGranted()
+                            //     ?
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    CallScreen(call: widget.call),
+                              ),
+                            );
+                                // : {};
+                          }),
+                    ],
+                  )
+                ])));
+  }
+}
