e06b9760864764f0a7aa0b7e4e0684867f6a912d
taiwo_kenny <taiwokenny45@gmail.com>
2021-05-13 14:49:46 +0100

    setting up the call methods

--- lib/resources/call_methods.dart
+++ lib/resources/call_methods.dart
@@ -1,12 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:skype_clone/constants/strings.dart';
+import 'package:skype_clone/models/call.dart';
 
 class CallMethods {
   final CollectionReference callCollection = FirebaseFirestore.instance
       .collection(CALL_COLLECTION);
 
-  Future<bool> makeCall() async{
+  Future<bool> makeCall({Call call}) async{
+    try{
+      Map<String, dynamic> hasDialedMap = call.toMap(call);
+      call.hasDialled = false;
+      Map<String, dynamic> hasNotDialedMap = call.toMap(call);
+      await callCollection.doc(call.callerId).set(hasDialedMap);
+      await callCollection.doc(call.receiverId).set(hasNotDialedMap);
+      return true;
+    }catch(e){
+      print(e);
+      return false;
+    }
+  }
 
+  Future<bool> endCall({Call call}) async {
+    try {
+      await callCollection.doc(call.callerId).delete();
+      await callCollection.doc(call.receiverId).delete();
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
   }
 
 }
